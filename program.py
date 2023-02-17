17377daaf05d9b6bd42ef6384de8d53dd2956b7c
MATEUSZZIUZIA <ziuziamateusz91@gmail.com>
2023-02-17 21:23:16 +0100

    sex selections

--- program.py
+++ program.py
@@ -1,5 +1,10 @@
 #v1 +++
-wiek =input("Podaj wiek uzytkownika")
+wiek =input("Podaj wiek uzytkownika\n")
+plec = int(input("Podaj plec\n"))
+if wiek >=30 and plec =="kobieta":
+    print("Pierwszy aperol spiritz gratis")
+else:
+    exit("Niestety nie przysługują ci gratisy")
 if wiek.isdigit() == False:
     exit("Wiek musi byc liczba calkowita")
 wiek=int(wiek)
